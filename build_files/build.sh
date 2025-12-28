f015b4acc580308dc8dd6b1eb261e770dcf3d0e6
kokoDroid <hogarfield@yahoo.com>
2025-12-28 22:31:14 +0100

    Add local printer rpms and vscode

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,6 +3,27 @@
 set -ouex pipefail
 
 ### Install packages
+#!/usr/bin/env bash
+
+# Add Microsoft GPG key
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Add VS Code repo
+cat << 'EOF' > /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+# Install VS Code
+dnf5 install -y code
+dnf5 install -y /rpms/*.rpm
+
+# Clean
+#dnf5 clean all
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
@@ -11,6 +32,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
+dnf5 clean all
 
 # Use a COPR Example:
 #
