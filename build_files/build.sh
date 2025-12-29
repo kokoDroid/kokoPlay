9f13fc59df45b0bc24086c22252f48edf66c82e0
kokoDroid <hogarfield@yahoo.com>
2025-12-29 18:03:37 +0100

    brave added fixed

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,11 +2,14 @@
 
 set -ouex pipefail
 
-# Add Brave repo
+# Import Brave signing key
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+# Add Brave repo (CORRECT URL)
 cat > /etc/yum.repos.d/brave-browser.repo << 'EOF'
 [brave-browser]
 name=Brave Browser
-baseurl=https://brave-browser-rpm-release.s3.brave.com/
+baseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64/
 enabled=1
 gpgcheck=1
 gpgkey=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
