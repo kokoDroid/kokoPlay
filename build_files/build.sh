eb3a9c189a859910e2ac7420a9100dbe3351630e
kokoDroid <hogarfield@yahoo.com>
2025-12-29 23:49:34 +0100

    pongo reinstall2

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,8 +20,8 @@ rpm --import https://download.docker.com/linux/fedora/gpg
 # Install Brave
 #dnf5 install -y brave-browser
 dnf5 install -y adw-gtk3-theme abattis-cantarell-fonts
-dnf5 reinstall -y pango pango-devel
-
+dnf5 reinstall -y pango
+dnf5 install -y pango-devel
 #dnf5 install -y gtk4
 
 
