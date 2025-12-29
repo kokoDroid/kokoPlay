53d999efb1be10ece7c3c3ee6fc160e1009a2493
kokoDroid <hogarfield@yahoo.com>
2025-12-29 20:38:17 +0100

    brave added fixed5

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,6 +18,8 @@ EOF
 # Install Brave
 dnf5 install -y brave-browser
 dnf5 install -y adw-gtk3-theme abattis-cantarell-fonts
+dnf5 install -y pango pango-graphite2 pango-modules cairo cairo-gobject
+
 #dnf5 install -y gtk4
 
 
