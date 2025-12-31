fa49ad45b6170d025bf01048e67d0bb3325124a8
kokoDroid <hogarfield@yahoo.com>
2025-12-31 18:15:46 +0100

    initramfs fix4

--- build_files/build-initramfs.sh
+++ build_files/build-initramfs.sh
@@ -13,21 +13,24 @@ log "Building initramfs"
 
 KERNEL_VERSION="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' kernel)"
 
-# Build directory (Bazzite staging root)
+
+# Staging root for the image
 BUILD_ROOT="/tmp/image-root"
+
+# Ensure module directory exists inside the image root
 mkdir -p "$BUILD_ROOT/usr/lib/modules/$KERNEL_VERSION"
 
-# Generate initramfs into the staging tree
+# Build initramfs INTO the image root (not the host)
 /usr/bin/dracut \
+  --installroot "$BUILD_ROOT" \
   --no-hostonly \
   --kver "$KERNEL_VERSION" \
   --reproducible \
   --zstd \
-  -v \
   --add ostree \
-  -f "$BUILD_ROOT/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+  -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 
+# Secure permissions
 chmod 0600 "$BUILD_ROOT/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
-
 
 log "Build completed"
