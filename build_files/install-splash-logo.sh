ebb9910db9b75ad49ff9d1161d4dc17827fd6260
kokoDroid <hogarfield@yahoo.com>
2026-01-05 00:18:31 +0100

    splash logo fix 3

--- build_files/install-splash-logo.sh
+++ build_files/install-splash-logo.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/bash
 
 # Copy theme
+mkdir -p /usr/local/share/plasma/look-and-feel
+cp -r /usr/share/plasma/look-and-feel/com.valve.vapor.desktop \
+      /usr/local/share/plasma/look-and-feel/
 
-mkdir -p /etc/plasma/look-and-feel
-cp -r /usr/share/plasma/look-and-feel/com.valve.vapor.desktop /etc/plasma/look-and-feel/
+curl -L -o \
+/usr/local/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/splash/images/kokoplay-logo.svgz \
+https://raw.githubusercontent.com/kokoDroid/kokoPlay/main/repo_files/kokoplay-logo.svgz
 
-# Paths
-LOOKANDFEEL=/etc/plasma/look-and-feel/com.valve.vapor.desktop
-SPLASH=$LOOKANDFEEL/contents/splash
+SPLASH=/usr/local/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/splash
+
+sed -i '/source:.*logo/ s|source:.*|source: "images/kokoplay-logo.svgz"|' \
+"$SPLASH/Splash.qml"
 
 
-# Download custom logo from GitHub
-curl -L -o $SPLASH/images/kokoplay-logo.svgz \
-    https://raw.githubusercontent.com/kokoDroid/kokoPlay/main/repo_files/kokoplay-logo.svgz
-
-# Update Splash.qml to point to new logo
-sed -i '/source:.*logo/ s|source:.*|source: "images/kokoplay-logo.svgz"|' "$SPLASH/Splash.qml"
