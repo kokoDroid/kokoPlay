529046f016f0c22c61b1c4612951d640e13f41b7
kokoDroid <hogarfield@yahoo.com>
2026-01-14 21:59:52 +0100

    transparency and icon fix

--- system_files/usr/share/plasma/look-and-feel/com.kokoplay.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ system_files/usr/share/plasma/look-and-feel/com.kokoplay.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -3,6 +3,19 @@ loadTemplate("org.kde.plasma.desktop.defaultPanel");
 var panel = panels()[0];
 
 if (panel) {
+    
+    var widgets = panel.widgets();
+
+    for (var i = 0; i < widgets.length; i++) {
+        var w = widgets[i];
+
+        if (w.type === "org.kde.plasma.kickoff" ||
+            w.type === "org.kde.plasma.kicker") {
+
+            w.writeConfig("icon", "kokoplay-menu");
+        }
+    }
+
     var launcher = panel.addWidget("org.kde.plasma.kickerdash");
     launcher.writeConfig("icon", "kokoplay-menu");
 
