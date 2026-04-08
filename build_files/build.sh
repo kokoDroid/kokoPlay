42ae3daad14ebf600fa5bef2c1949a8571b3d435
kokoDroid <hogarfield@yahoo.com>
2026-04-08 20:31:06 +0200

    hostname3

--- build_files/build.sh
+++ build_files/build.sh
@@ -90,5 +90,7 @@ ln -s /dev/null /etc/systemd/user/app-megasync@autostart.service
 sed -i 's|^gpgkey=file:///etc/pki/rpm-gpg/|gpgkey=https://raw.githubusercontent.com/kokoDroid/kokoPlay/main/disk_config/rpm-gpg/|' /etc/yum.repos.d/terra*.repo
 
 
+hostnamectl set-hostname kokoplay
+
 
 systemctl enable podman.socket
